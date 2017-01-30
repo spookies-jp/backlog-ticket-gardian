6d284035beb6567cae3b414cffe13d19d1af7e55
Masayuki Morisaki <masayuki144@gmail.com>
2017-01-30 16:39:02 +0900

    utility用のモジュールを作成

---
+++ utils.js
@@ -0,0 +1,34 @@
+var utils = {
+
+  /**
+   * 存在する値ならtrue
+   */
+  existy: function(v) {
+
+    if (v === undefined) {
+      return false;
+    }
+    return (v != null)
+  },
+
+  /**
+   * false判定されるもの
+   */
+  falsy: function(v) {
+
+    if (this.existy(v)) {
+
+      if (v === 'false') {
+        return true;
+      }
+      if (v) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+if (typeof module !== 'undefined' && module && module.exports) {
+  module.exports = utils;
+}

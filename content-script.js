b975e9ddd174f8c141e93e320dad00e552280f91
Masayuki Morisaki <masayuki144@gmail.com>
2017-01-31 11:46:13 +0900

    **予定** **実績** のイベント処理を実装

--- content-script.js
+++ content-script.js
@@ -26,6 +26,13 @@ var BacklogTicketGuardian = {
     $(status_elements[1]).click(self.onChangeInProgress(guard_element));
     $(status_elements[2]).click(self.onChangeResolved(guard_element));
     $(status_elements[3]).click(self.onChangeClosed(guard_element));
+
+    // 予定/実績 の入力によるイベント
+    var div = $('div.change-statuses-properties-item.-estimated-hours')
+    var est = div.children('input[name="switchStatusIssue.estimatedHours"]');
+    var act = div.children('input[name="switchStatusIssue.actualHours"]');
+    est.blur(self.onBlurHours(guard_element));
+    act.blur(self.onBlurHours(guard_element));
   },
 
 
@@ -111,31 +118,66 @@ var BacklogTicketGuardian = {
     var self = BacklogTicketGuardian;
     return self.onChangeResolved(guard_element);
   },
+
+
+
+  //  on (予定|実績)から離れる
+  //
+  //    if status == 処理中
+  //
+  //      if 予定 == empty
+  //
+  //        `登録` ボタンをdisable
+  //
+  //      else
+  //
+  //        `登録` enable
+  //
+  //    else if status == 処理済
+  //
+  //      if 予定 == empty || 実績 == empty
+  //
+  //        `登録` disable
+  //
+  //      else
+  //
+  //        `登録` enable
+  //
+  onBlurHours: function(guard_element) {
+
+    var div = $('div.change-statuses-properties-item.-estimated-hours')
+    var est = div.children('input[name="switchStatusIssue.estimatedHours"]');
+    var act = div.children('input[name="switchStatusIssue.actualHours"]');
+
+    return function(e) {
+
+      var status_elements = $(".comment-editor__radio-item");
+      var status = status_elements.children('input:checked').prop('value');
+
+      guard_element.hide();
+      switch (Number(status)) {
+        case 1: // 未対応
+          break;
+
+        case 2: // 処理中
+          if (utils.empty(est.prop('value'))) {
+            guard_element.show();
+          }
+          break;
+
+        case 3: // 処理済
+        case 4: // 完了
+          if (utils.empty(est.prop('value')) || utils.empty(act.prop('value'))) {
+            guard_element.show();
+          }
+          break;
+
+        default:
+          break;
+      }
+
+    };
+  },
 };
 
 BacklogTicketGuardian.init();
-
-// ステータス毎のDOMにonClickイベントを登録して制御
-
-//  on (予定|実績)から離れる
-//
-//    if status == 処理中
-//
-//      if 予定 == empty
-//
-//        `登録` ボタンをdisable
-//
-//      else
-//
-//        `登録` enable
-//
-//    else if status == 処理済
-//
-//      if 予定 == empty || 実績 == empty
-//
-//        `登録` disable
-//
-//      else
-//
-//        `登録` enable
-//
